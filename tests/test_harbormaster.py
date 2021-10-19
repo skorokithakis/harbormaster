02cfebeb91a3d5c440dd06a3dc48dadfeb5a5b96
Stavros Korokithakis <hi@stavros.io>
2021-10-19 14:55:06 +0300

    test: Refactor tests a bit more

--- tests/test_harbormaster.py
+++ tests/test_harbormaster.py
@@ -1,4 +1,8 @@
-import tempfile
+import os
+from contextlib import contextmanager
+from pathlib import Path
+from typing import Iterable
+from typing import Tuple
 from unittest.mock import patch
 
 import git
@@ -7,7 +11,18 @@ from click.testing import CliRunner
 from docker_harbormaster import cli
 
 
-def patched_run():
+@contextmanager
+def _chdir(path: Path):
+    """Sets the cwd within the context."""
+    origin = Path().absolute()
+    try:
+        os.chdir(path)
+        yield
+    finally:
+        os.chdir(origin)
+
+
+def _patched_run():
     """Mock _run_command_full so that we can get the commands."""
     rcf = cli._run_command_full
 
@@ -25,46 +40,62 @@ def patched_run():
     return inner, commands
 
 
+def create_repository(path: Path, contents: Iterable[Tuple[str, str]]):
+    """
+    Create a repository with the specified contents.
+
+    Contents should be an iterable of (filename, contents) tuples.
+    """
+    repo = git.Repo.init(path)
+    with _chdir(path):
+        for filename, content in contents:
+            with open(filename, "w") as outfile:
+                outfile.write(content)
+            repo.index.add(filename)
+    repo.index.commit("Initial commit")
+
+
 def test_one_app(tmp_path):
-    hm_yml = f"""
-            apps:
-              myapp:
-                # The git repository URL to clone.
-                url: {tmp_path}
-        """
-    with (tmp_path / "harbormaster.yml").open("w") as hm_yml_file:
-        hm_yml_file.write(hm_yml)
+    create_repository(
+        tmp_path / "harbormaster",
+        (
+            (
+                "harbormaster.yml",
+                f"""
+                apps:
+                  myapp:
+                    url: {tmp_path}/harbormaster
+                """,
+            ),
+            ("harbormaster2.yml", "hi"),
+            (
+                "docker-compose.yml",
+                """
+                version: "3.9"
+                services:
+                  web:
+                    build: .
+                    ports:
+                      - "5000:5000"
+                  redis:
+                    image: "redis:alpine"
+                """,
+            ),
+        ),
+    )
 
-    dc_yml = """
-            version: "3.9"
-            services:
-              web:
-                build: .
-                ports:
-                  - "5000:5000"
-              redis:
-                image: "redis:alpine"
-        """
-
-    with (tmp_path / "docker-compose.yml").open("w") as hm_yml_file:
-        hm_yml_file.write(dc_yml)
-
-    r = git.Repo.init(tmp_path)
-    r.index.add(["harbormaster.yml", "docker-compose.yml"])
-    r.index.commit("initial commit")
-
-    fn, commands = patched_run()
-    with tempfile.TemporaryDirectory() as wdir, patch(
-        "docker_harbormaster.cli._run_command_full", side_effect=fn
-    ):
+    fn, commands = _patched_run()
+    working_dir = tmp_path / "working_dir"
+    working_dir.mkdir()
+    with patch("docker_harbormaster.cli._run_command_full", side_effect=fn):
         runner = CliRunner()
         result = runner.invoke(
             cli.cli,
             [
                 "--config",
-                f"{tmp_path}/harbormaster.yml",
+                f"{tmp_path}/harbormaster/harbormaster.yml",
                 "--working-dir",
-                wdir,
+                str(working_dir),
             ],
         )
 
