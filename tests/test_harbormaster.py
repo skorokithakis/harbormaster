028bba5e04f8e7dd6f4a941c67f6023741385752
Stavros Korokithakis <hi@stavros.io>
2021-10-19 21:47:49 +0300

    test: Test for different branches

--- tests/test_harbormaster.py
+++ tests/test_harbormaster.py
@@ -1,12 +1,15 @@
-from unittest.mock import patch
+from pathlib import Path
+from typing import Dict
 
 import pytest
-from click.testing import CliRunner
-from utils import create_repository
-from utils import mkdir
-from utils import patched_run
+from utils import Repository
+from utils import run_harbormaster
 
 from docker_harbormaster import cli
+
+# Tarry not.
+cli.RETRY_WAIT_SECONDS = 0
+cli.MAX_GIT_NETWORK_ATTEMPTS = 1
 
 
 @pytest.fixture()
@@ -14,36 +17,35 @@ def repos(tmp_path):
     """Set up the required repositories for the tests."""
     repos = {}
 
-    # Create the app repo.
-    repos["app1"] = create_repository(
-        tmp_path / "app1",
-        (
-            (
-                "docker-compose.yml",
-                """
-                version: "3.9"
-                services:
-                  web:
-                    build: .
-                    ports:
-                      - "5000:5000"
-                  redis:
-                    image: "redis:alpine"
-                """,
-            ),
-        ),
+    # Create the app repo and add an app.
+    repo = Repository("apps", tmp_path)
+    repo.add_files(
+        (("docker-compose.yml", "services:\n web:\n  image: app"),),
     )
 
+    # Add app1.
+    repo.checkout("app1")
+    repo.add_files(
+        (("docker-compose.yml", "services:\n web:\n  image: app"),),
+    )
+
+    repo.checkout("app2")
+    repo.add_files(
+        (("docker-compose.yml", "services:\n web:\n  image: app"),),
+    )
+
+    repos["apps"] = repo
+
     # Create the Harbormaster config repo.
-    repos["config"] = create_repository(
-        tmp_path / "harbormaster",
+    repos["config"] = Repository("config", tmp_path)
+    repos["config"].add_files(
         (
             (
                 "harbormaster.yml",
                 f"""
                 apps:
                   myapp:
-                    url: {tmp_path}/app1/
+                    url: {repos['apps'].path}
                 """,
             ),
         ),
@@ -51,25 +53,69 @@ def repos(tmp_path):
     return repos
 
 
-def test_one_app(tmp_path, repos):
+def test_one_app(tmp_path: Path, repos: Dict[str, Repository]):
     """Check a single-app scenario."""
-    fn, commands = patched_run()
-    with patch("docker_harbormaster.cli._run_command_full", side_effect=fn):
-        runner = CliRunner()
-        result = runner.invoke(
-            cli.cli,
-            [
-                "--config",
-                f"{repos['config'].working_tree_dir}/harbormaster.yml",
-                "--working-dir",
-                str(mkdir(tmp_path / "working_dir")),
-            ],
-        )
+    result, output = run_harbormaster(tmp_path, repos)
 
-        assert result.exit_code == 0
-        assert result.output
-        assert commands == [
-            "/usr/bin/env docker-compose -f docker-compose.yml ps --services --filter status=running",
-            "/usr/bin/env docker-compose -f docker-compose.yml pull",
-            "/usr/bin/env docker-compose -f docker-compose.yml up --remove-orphans --build -d",
-        ]
+    assert result.exit_code == 0
+    assert result.output
+    assert output["commands"] == [
+        "/usr/bin/env docker-compose -f docker-compose.yml ps --services --filter status=running",
+        "/usr/bin/env docker-compose -f docker-compose.yml pull",
+        "/usr/bin/env docker-compose -f docker-compose.yml up --remove-orphans --build -d",
+    ]
+
+
+def test_branches(tmp_path: Path, repos: Dict[str, Repository]):
+    """Check the scenario where every app is a branch in the same repo."""
+    # Create a config that runs apps in branches.
+    repos["config"].add_files(
+        (
+            (
+                "harbormaster.yml",
+                f"""
+                apps:
+                  app1:
+                    url: {repos['apps'].path}
+                    branch: app1
+                  app2:
+                    url: {repos['apps'].path}
+                    branch: app2
+                """,
+            ),
+        ),
+    )
+
+    result, output = run_harbormaster(tmp_path, repos)
+
+    assert result.exit_code == 0
+    assert result.output
+    assert output["clone_or_pull"] == {"app1": True, "app2": True}
+    assert output["commands"] == [
+        "/usr/bin/env docker-compose -f docker-compose.yml ps --services --filter status=running",
+        "/usr/bin/env docker-compose -f docker-compose.yml pull",
+        "/usr/bin/env docker-compose -f docker-compose.yml up --remove-orphans --build -d",
+        "/usr/bin/env docker-compose -f docker-compose.yml ps --services --filter status=running",
+        "/usr/bin/env docker-compose -f docker-compose.yml pull",
+        "/usr/bin/env docker-compose -f docker-compose.yml up --remove-orphans --build -d",
+    ]
+
+    # Change the app in one branch and ensure the other one didn't restart.
+    repos["apps"].checkout("app2")
+    repos["apps"].add_files(
+        (("test", "Whatever"),),
+    )
+
+    result, output = run_harbormaster(tmp_path, repos)
+
+    assert result.exit_code == 0
+    assert result.output
+    assert output["clone_or_pull"] == {"app1": False, "app2": True}
+    assert output["commands"] == [
+        "/usr/bin/env docker-compose -f docker-compose.yml ps --services --filter status=running",
+        "/usr/bin/env docker-compose -f docker-compose.yml pull",
+        "/usr/bin/env docker-compose -f docker-compose.yml up --remove-orphans --build -d",
+        "/usr/bin/env docker-compose -f docker-compose.yml ps --services --filter status=running",
+        "/usr/bin/env docker-compose -f docker-compose.yml pull",
+        "/usr/bin/env docker-compose -f docker-compose.yml up --remove-orphans --build -d",
+    ]
--- tests/utils.py
+++ tests/utils.py
@@ -1,18 +1,16 @@
 import os
 from contextlib import contextmanager
 from pathlib import Path
+from typing import Any
+from typing import Dict
 from typing import Iterable
 from typing import Tuple
+from unittest.mock import patch
 
 import git
+from click.testing import CliRunner
 
 from docker_harbormaster import cli
-
-
-def mkdir(path: Path) -> Path:
-    """Create a directory out of a path and return that path."""
-    path.mkdir(exist_ok=True)
-    return path
 
 
 @contextmanager
@@ -26,7 +24,40 @@ def chdir(path: Path):
         os.chdir(origin)
 
 
-def patched_run():
+class Repository:
+    def __init__(self, name: str, root_dir: Path, branch="master"):
+        self._name = name
+        self._root_dir = root_dir
+        self._repo = git.Repo.init(root_dir / name)
+        self.checkout(branch)
+
+    @property
+    def path(self) -> Path:
+        """Return the path of the repo on disk."""
+        return Path(str(self._repo.working_tree_dir))
+
+    def add_files(self, contents: Iterable[Tuple[str, str]]) -> None:
+        """Add a bunch of files to a repository and commit."""
+        with chdir(self.path):
+            for filename, content in contents:
+                with open(filename, "w") as outfile:
+                    outfile.write(content)
+                self._repo.index.add(filename)
+        self._repo.index.commit("Commit")
+
+    def checkout(self, rev: str) -> None:
+        """Check out a given revision."""
+        refs = [x.name for x in self._repo.references]
+        self._repo.git.checkout(rev, b=rev not in refs)
+
+
+def mkdir(path: Path) -> Path:
+    """Create a directory out of a path and return that path."""
+    path.mkdir(exist_ok=True)
+    return path
+
+
+def _patched_run():
     """Mock _run_command_full so that we can get the commands."""
     rcf = cli._run_command_full
 
@@ -44,22 +75,32 @@ def patched_run():
     return inner, commands
 
 
-def create_repository(
-    path: Path,
-    contents: Iterable[Tuple[str, str]],
-    branch="master",
-) -> git.Repo:
-    """
-    Create a repository with the specified contents.
+def run_harbormaster(
+    tmp_path: Path, repos: Dict[str, Repository]
+) -> Tuple[Any, Dict[str, Any]]:
+    """I'm terribly sorry about this, it was the only way."""
+    rcf_mock, commands = _patched_run()
+    fn_cop = cli.App.clone_or_pull
+    # Prepare a list of the functions' outputs.
+    output: Dict[str, Dict[str, bool]] = {"clone_or_pull": {}}
 
-    Contents should be an iterable of (filename, contents) tuples.
-    """
-    repo = git.Repo.init(path)
-    repo.git.checkout(b=branch)
-    with chdir(path):
-        for filename, content in contents:
-            with open(filename, "w") as outfile:
-                outfile.write(content)
-            repo.index.add(filename)
-    repo.index.commit("Initial commit")
-    return repo
+    def cop_mock(self, *args, **kwargs):
+        """A clone_or_pull mock."""
+        retval = fn_cop(self, *args, **kwargs)
+        output["clone_or_pull"][self.id] = retval
+        return retval
+
+    with patch("docker_harbormaster.cli._run_command_full", side_effect=rcf_mock):
+        with patch.object(cli.App, "clone_or_pull", cop_mock):
+            runner = CliRunner()
+            result = runner.invoke(
+                cli.cli,
+                [
+                    "--config",
+                    f"{repos['config'].path}/harbormaster.yml",
+                    "--working-dir",
+                    str(mkdir(tmp_path / "working_dir")),
+                ],
+            )
+    output["commands"] = commands
+    return result, output
