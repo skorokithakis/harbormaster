1c530893427335f2a7fbdcbeb0a02241c1ec3b4d
Stavros Korokithakis <hi@stavros.io>
2021-11-02 14:33:03 +0200

    test: Add more tests

--- tests/test_harbormaster.py
+++ tests/test_harbormaster.py
@@ -1,3 +1,4 @@
+import random
 from pathlib import Path
 from typing import Dict
 
@@ -16,39 +17,45 @@ cli.MAX_GIT_NETWORK_ATTEMPTS = 1
 def repos(tmp_path):
     """Set up the required repositories for the tests."""
     repos = {}
-    dockerfile = (
-        (
-            "docker-compose.yml",
-            """
+
+    def dockerfile():
+        # We need to add a random number in the Dockerfile, otherwise git produces the
+        # exact same hashes for both repos, probably because it only considers seconds
+        # for the timestamps.
+        return (
+            (
+                "docker-compose.yml",
+                f"""
             services:
               web:
                 image: app
+                random_number: {random.random()}
                 volumes:
                   - {{ HM_DATA_DIR }}/data:/data
             """,
-        ),
-    )
+            ),
+        )
 
     # Create the app repo and add an app.
     repo = Repository("apps", tmp_path)
-    repo.add_files(dockerfile)
+    repo.add_files(dockerfile())
     # Add app1.
     repo.checkout("app1")
-    repo.add_files(dockerfile)
+    repo.add_files(dockerfile())
     # Add app2.
     repo.checkout("app2")
-    repo.add_files(dockerfile)
+    repo.add_files(dockerfile())
     repos["apps"] = repo
 
     # Create another app repo and add an app.
     repo = Repository("apps2", tmp_path)
-    repo.add_files(dockerfile)
+    repo.add_files(dockerfile())
     # Add app1.
     repo.checkout("app1")
-    repo.add_files(dockerfile)
+    repo.add_files(dockerfile())
     # Add app2.
     repo.checkout("app2")
-    repo.add_files(dockerfile)
+    repo.add_files(dockerfile())
     repos["apps2"] = repo
 
     # Create the Harbormaster config repo.
@@ -268,4 +275,46 @@ def test_changing_remotes(tmp_path: Path, repos: Dict[str, Repository]):
 
     assert result.exit_code == 0
     assert result.output
-    assert output["restarted_apps"] == set()
+    assert output["restarted_apps"] == {"app1", "app2"}
+
+
+def test_changing_branches(tmp_path: Path, repos: Dict[str, Repository]):
+    repos["config"].add_files(
+        (
+            (
+                "harbormaster.yml",
+                f"""
+                apps:
+                  app1:
+                    url: {repos['apps'].path}
+                """,
+            ),
+        ),
+    )
+
+    result, output = run_harbormaster(tmp_path, repos)
+
+    assert result.exit_code == 0
+    assert result.output
+    assert output["restarted_apps"] == {"app1"}
+
+    # Change remotes.
+    repos["config"].add_files(
+        (
+            (
+                "harbormaster.yml",
+                f"""
+                apps:
+                  app1:
+                    url: {repos['apps'].path}
+                    branch: app1
+                """,
+            ),
+        ),
+    )
+
+    result, output = run_harbormaster(tmp_path, repos)
+
+    assert result.exit_code == 0
+    assert result.output
+    assert output["restarted_apps"] == {"app1"}
