69041db67ec8b7b681cc4ecc5bde92c4473d99b9
Stavros Korokithakis <hi@stavros.io>
2023-07-26 23:58:20 +0300

    doc: Remove static setting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,6 @@ exclude_patterns: List[str] = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
 
 myst_enable_extensions = [
     "colon_fence",
